86a1fa8dc9562aa3071475b5f050e99709a06988
CrazyRabbit <crazyrabbit0@outlook.com>
2022-04-19 17:34:12 +0300

    New File (Stop Adobe Apps)

---
+++ Stop Adobe Apps.ps1
@@ -0,0 +1,60 @@
+
+#--------------- Variables ---------------
+
+$textPrefix = " "
+$scriptTitle = (Get-Item $PSCommandPath).Basename
+
+#--------------- Main Code ---------------
+
+function main {
+	param ([String[]] $argz)
+	
+	#runWithAdminRights
+	showTitle $scriptTitle
+	Get-Process "Acro*" | Stop-Process -Force -PassThru
+	Get-Process "Adobe*" | Stop-Process -Force -PassThru
+	""
+	showTitle "Finish"
+	quit
+}
+
+#--------------- Functions ---------------
+
+function runWithAdminRights {
+	if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+		Start-Process -Verb RunAs powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+		exit
+	}
+}
+
+function showTitle {
+	param (
+        [Parameter(Mandatory)]
+        [string]$title
+    )
+	""
+	"===============  $title  ==============="
+	""
+}
+
+function quit {
+	param (
+        [ValidateNotNullOrEmpty()]
+        [string]$text = "${textPrefix}Exiting",
+		
+        [string]$runPath,
+		
+        [string]$runArgument
+    )
+	""
+	wait -text $text
+	if($runPath -ne $null) {
+		Start-Process $runPath $runArgument
+	}
+	""
+	exit
+}
+
+#--------------- Run Main Code ---------------
+
+main $args
