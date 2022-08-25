917471ddc06fbf9611c30dcd312a1c43b10b4787
CrazyRabbit <crazyrabbit0@outlook.com>
2022-08-25 14:09:30 +0300

    Added Script: Black Screen

---
+++ Black Screen.ps1
@@ -0,0 +1,21 @@
+﻿
+#--------------- Variables ---------------
+
+$debug = 0
+
+#--------------- Main Code ---------------
+
+function main {
+	param ([String[]] $argz)
+	
+	if($debug) { $argz;"" }
+	Add-Type -AssemblyName System.Windows.Forms
+	Start-Process -FilePath "X_80.contrast-white.png"
+	Start-Sleep 1
+	[System.Windows.Forms.SendKeys]::SendWait('{F5}')
+	if($debug) { "";pause }
+}
+
+#--------------- Run Main Code ---------------
+
+main $args
