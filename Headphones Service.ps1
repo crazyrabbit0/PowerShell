d8428b7630304b14f1c4969ab6fe400dca51c9f4
CrazyRabbit <crazyrabbit0@outlook.com>
2022-09-21 15:22:28 +0300

    Update Headphones Service.ps1
    Removed unnecessary loop
    Find Bluetooth Devices automatically
    Added restartEveryHour

--- Headphones Service.ps1
+++ Headphones Service.ps1
@@ -2,65 +2,74 @@
 #--------------- Variables ---------------
 
 $debug = 0
-$device_list = @("ZV Headphones", "Evi Headphones")
+#$device_list = @("ZV Headphones", "Evi Headphones")
 $program_path = "$env:cr\Programs\Portables\Audio Repeater MME + KS\audiorepeater_ks.exe"
+$powershell_path = "$env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
 
 #--------------- Main Code ---------------
 
-function main {
+function main
+{
 	param ([String[]] $argz)
 	
-	runHiddenWithAdminRights $argz
+	runAsAdminHidden $argz
     oneInstanceMode
-    $program_file = Split-Path $program_path -Leaf
-    $program_directory = Split-Path $program_path -Resolve
 	while($true)
 	{
-        $bluetooth_devices = Get-PnpDevice -Class "Bluetooth"
-		foreach($device_name in $device_list)
-		{
-			if($debug) { $device_name;"" }
-			#
-            # DEVPKEY_Device_DevNodeStatus (Data)
-			#    25190410 = Connected
-			#    58744842 = Disconnected
-			#
-			# DEVPKEY_Bluetooth_LastConnectedTime (Type)
-			#    Empty = Connected
-			#    Not Empty = Disconnected
-            #
-			#$device_connected = ($bluetooth_devices | Where-Object {$_.FriendlyName -eq $device_name}  | Get-PnpDeviceProperty -KeyName "DEVPKEY_Device_DevNodeStatus").Data -eq 25190410
-			$device_connected = ($bluetooth_devices | Where-Object {$_.FriendlyName -eq $device_name}  | Get-PnpDeviceProperty -KeyName "DEVPKEY_Bluetooth_LastConnectedTime").Type -eq "Empty"
-            $program_running = Get-CimInstance Win32_Process -Filter "name = ""$program_file""" | Where CommandLine -Like "*$device_name*"
-			if(-not $device_connected -or $program_running) { continue }
-			if($debug) { $program_running;"" }
-			Start-Process -FilePath "taskkill" -ArgumentList "/pid $($program_running.ProcessId)" -WindowStyle Hidden
-			Start-Sleep -Seconds 1.5 # Delay for device to be ready after connection
-			Start-Process -FilePath "$env:comspec" -ArgumentList "/c start /min """" ""$program_file"" /Input:""VB-Audio Point"" /Output:""$device_name Stereo"" /OutputPrefill:70 /SamplingRate:44100 /AutoStart" -WorkingDirectory $program_directory -WindowStyle Hidden
-		}
-        Start-Sleep -Seconds 1
+		restartEveryHour
+        $bluetooth_devices = Get-PnpDevice | Where Description -eq "Bluetooth Device"
+		#
+		# DEVPKEY_Device_DevNodeStatus (Data)
+		#    25190410 = Connected
+		#    58744842 = Disconnected
+		#
+		# DEVPKEY_Bluetooth_LastConnectedTime (Type)
+		#    Empty = Connected
+		#    FileTime = Disconnected
+		#
+		$connected_device = $bluetooth_devices | Get-PnpDeviceProperty | Where-Object {$_.KeyName -eq "DEVPKEY_Device_DevNodeStatus" -and $_.Data -eq "25190410"}
+		if(-not $connected_device) {continue}
+		if($debug) {$connected_device;""}
+		$device_name = (Get-PnpDevice -InstanceId $connected_device.InstanceId).FriendlyName
+		$program_running = Get-CimInstance Win32_Process | Where CommandLine -Like """$program_path""*$device_name*"
+		if($program_running) {continue}
+		if($debug) {$program_running;""}
+		Start-Process -FilePath "taskkill" -ArgumentList "/pid $($program_running.ProcessId)" -WindowStyle Hidden
+		Start-Sleep -Seconds 1.5 # Delay for device to be ready after connection
+		Start-Process -FilePath "$env:comspec" -ArgumentList "/c start /min """" ""$program_path"" /Input:""VB-Audio Point"" /Output:""$device_name Stereo"" /OutputPrefill:70 /SamplingRate:44100 /AutoStart" -WindowStyle Hidden
 		#if($debug) { "";pause }
 	}
 }
 
 #--------------- Functions ---------------
 
-function runHiddenWithAdminRights
+function runAsAdminHidden
 {
     param ([String[]] $argz)
 	
 	$process_is_admin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-	$process_is_hidden = Get-CimInstance Win32_Process -Filter "name = ""powershell.exe""" | Where ProcessId -eq $PID | Where CommandLine -Like "*-WindowStyle Hidden*"
-	if($process_is_admin -and $process_is_hidden) { return }
-	Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
+	$process_is_hidden = Get-CimInstance Win32_Process | Where-Object {$_.ProcessId -eq $PID -and $_.CommandLine -Like "*-WindowStyle Hidden*"}
+	if($process_is_admin -and $process_is_hidden) {return}
+	Start-Process -Verb RunAs -FilePath "$powershell_path" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
 	exit
 }
 
 function oneInstanceMode
 {
-	$all_instances = Get-CimInstance Win32_Process -Filter "name = ""powershell.exe""" | Where CommandLine -Like "*$PSCommandPath*"
-	$all_instances | Where ProcessId -ne $PID | foreach { Stop-Process -Id $_.ProcessId }
+	$other_instances = Get-CimInstance Win32_Process | Where-Object {$_.ProcessId -ne $PID -and $_.CommandLine -Like """$powershell_path""*""$PSCommandPath""*"}
+	$other_instances | foreach {Stop-Process -Id $_.ProcessId}
 }
+
+function restartEveryHour
+{
+	$current_time = Get-Date
+	$process_start_time = (Get-Process -id $PID).StartTime
+	$process_runs_less_than_an_hour = ($current_time - $process_start_time).TotalHours < 1
+	If($process_runs_less_than_an_hour) {return}
+	Start-Process -Verb RunAs -FilePath "$powershell_path" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
+	exit
+}
+
 
 #--------------- Run Code ---------------
 
