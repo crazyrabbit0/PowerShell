cd33c69ff0192a4d59dff982e0bf3b03a58f079e
CrazyRabbit <crazyrabbit0@outlook.com>
2022-09-22 12:41:29 +0300

    Added Headphone Service as a Task

--- Headphone Service.ps1
+++ Headphone Service.ps1
@@ -2,9 +2,7 @@
 #--------------- Variables ---------------
 
 $debug = 0
-#$device_list = @("ZV Headphones", "Evi Headphones")
 $program_path = "$env:cr\Programs\Portables\Audio Repeater MME + KS\audiorepeater_ks.exe"
-$powershell_path = "$env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
 
 #--------------- Main Code ---------------
 
@@ -13,11 +11,12 @@ function main
 	param ([String[]] $argz)
 	
 	runAsAdminHidden $argz
-    oneInstanceMode
+	oneInstanceMode
 	while($true)
 	{
-		restartEveryHour
-        $bluetooth_devices = Get-PnpDevice | Where Description -eq "Bluetooth Device"
+		#restartEveryHour
+		Start-Sleep -Seconds 1
+		$bluetooth_devices = Get-PnpDevice | Where Description -eq "Bluetooth Device"
 		#
 		# DEVPKEY_Device_DevNodeStatus (Data)
 		#    25190410 = Connected
@@ -34,7 +33,7 @@ function main
 		$program_running = Get-CimInstance Win32_Process | Where CommandLine -Like """$program_path""*$device_name*"
 		if($program_running) {continue}
 		if($debug) {$program_running;""}
-		Start-Process -FilePath "taskkill" -ArgumentList "/pid $($program_running.ProcessId)" -WindowStyle Hidden
+		Start-Process -FilePath "taskkill" -ArgumentList "/im $(Split-Path $program_path -Leaf)" -WindowStyle Hidden
 		Start-Sleep -Seconds 1.5 # Delay for device to be ready after connection
 		Start-Process -FilePath "$env:comspec" -ArgumentList "/c start /min """" ""$program_path"" /Input:""VB-Audio Point"" /Output:""$device_name Stereo"" /OutputPrefill:70 /SamplingRate:44100 /AutoStart" -WindowStyle Hidden
 		#if($debug) { "";pause }
@@ -45,18 +44,18 @@ function main
 
 function runAsAdminHidden
 {
-    param ([String[]] $argz)
+	param ([String[]] $argz)
 	
 	$process_is_admin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 	$process_is_hidden = Get-CimInstance Win32_Process | Where-Object {$_.ProcessId -eq $PID -and $_.CommandLine -Like "*-WindowStyle Hidden*"}
 	if($process_is_admin -and $process_is_hidden) {return}
-	Start-Process -Verb RunAs -FilePath "$powershell_path" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
+	Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
 	exit
 }
 
 function oneInstanceMode
 {
-	$other_instances = Get-CimInstance Win32_Process | Where-Object {$_.ProcessId -ne $PID -and $_.CommandLine -Like """$powershell_path""*""$PSCommandPath""*"}
+	$other_instances = Get-CimInstance Win32_Process | Where-Object {$_.ProcessId -ne $PID -and $_.CommandLine -Like "*powershell.exe*""$PSCommandPath""*"}
 	$other_instances | foreach {Stop-Process -Id $_.ProcessId}
 }
 
@@ -66,10 +65,9 @@ function restartEveryHour
 	$process_start_time = (Get-Process -id $PID).StartTime
 	$process_runs_less_than_an_hour = ($current_time - $process_start_time).TotalHours -lt 1
 	If($process_runs_less_than_an_hour) {return}
-	Start-Process -Verb RunAs -FilePath "$powershell_path" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
+	Start-Process -Verb RunAs -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File ""$PSCommandPath"" $argz"
 	exit
 }
-
 
 #--------------- Run Code ---------------
 
