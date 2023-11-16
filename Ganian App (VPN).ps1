295a1b50a74ef3367483f961e343d2834b8cc7bf
CrazyRabbit <crazyrabbit0@outlook.com>
2023-11-17 00:20:59 +0200

    color typed messages

--- Ganian App (VPN).ps1
+++ Ganian App (VPN).ps1
@@ -21,29 +21,34 @@ function main
 	
 	"`n`n`t Αρχείο διευθύνσεων Windows:"
 	$hosts = @{
-		path	= "$env:WINDIR\System32\drivers\etc\hosts"
-		content	= $NULL
+		path		= "$env:WINDIR\System32\drivers\etc\hosts"
+		content		= $NULL
+		old_content	= $NULL
 	}
-	$hosts.content = Get-Content $hosts.path
+	$hosts.old_content	= Get-Content $hosts.path
+	$hosts.content		= $hosts.original_content
 	
 	$domains_with_ip = $hosts.content -Match $domain -Match $vpn_ip
 	if ($domains_with_ip.count -eq 0)
 	{
-		"`n`t   Προσθήκη νέας διεύθυνσης..."
+		Write-Host -NoNewLine -ForegroundColor Gray "`n`t • Προσθήκη νέας διεύθυνσης..."
 		$hosts.content += "`n$vpn_ip   $domain"
+		Write-Host -ForegroundColor Green ' √'
 	}
 	
 	$domains_without_ip = $hosts.content -Match $domain -NotMatch $vpn_ip
 	if ($domains_without_ip.count -gt 0)
 	{
-		"`n`t   Αφαίρεση παλαιών διευθύνσεων..."
+		Write-Host -NoNewLine -ForegroundColor Gray "`n`t • Αφαίρεση παλαιών διευθύνσεων..."
 		$hosts.content = $hosts.content -Replace ($domains_without_ip -join '|'), ''
+		Write-Host -ForegroundColor Green ' √'
 	}
 	
-	if ($hosts.content -ne (Get-Content $hosts.path))
+	if ($hosts.content -ne $hosts.old_content)
 	{
-		"`n`t   Αποθήκευση..."
+		Write-Host -NoNewLine -ForegroundColor Gray "`n`t • Αποθήκευση..."
 		$hosts.content -join "`n" -replace '\n{3,}', "`n`n" | Out-File $hosts.path
+		Write-Host -ForegroundColor Green ' √'
 	}
 	
 	"`n`n`n`t Εικονικό ιδιωτικό δίκτυο Radmin:"
@@ -55,28 +60,36 @@ function main
 	
 	if (-Not (Test-Path $radmin.path))
 	{
-		"`n`t   Λήψη..."
+		Write-Host -NoNewLin -ForegroundColor Graye "`n`t • Λήψη..."
 		$radmin_page = Invoke-RestMethod $radmin.url
 		$download_url = ($radmin_page | Select-String "<a href=""(.*?)"" class=""buttonDownload""").Matches.Groups[1].Value
 		$ProgressPreference = "SilentlyContinue"
 		Start-BitsTransfer -Source $download_url -Destination $radmin.download -DisplayName 'Λήψη...'
+		Write-Host -ForegroundColor Green ' √'
 		
-		"`n`t   Εγκατάσταση..."
+		Write-Host -NoNewLin -ForegroundColor Gray "`n`t • Εγκατάσταση..."
 		Start-Process $radmin.download '/VERYSILENT /NORESTART' -Wait
+		Write-Host -ForegroundColor Green ' √'
+		
+		Write-Host -NoNewLine -ForegroundColor Gray "`n`t • Αφαίρεση λήψης..."
 		Remove-Item $radmin.download
+		Write-Host -ForegroundColor Green ' √'
 	}
 	
 	if (Test-Path $radmin.path)
 	{
-		"`n`t   Εκκίνηση..."
+		Write-Host -NoNewLine -ForegroundColor Gray "`n`t • Εκκίνηση..."
 		Start-Process $radmin.path '/show'
+		Write-Host -ForegroundColor Green ' √'
 	}
 	
-	"`n`n`n`t Άνοιγμα πρoγράμματος περιήγησης..."
+	Write-Host -NoNewLine "`n`n`n`t Άνοιγμα πρoγράμματος περιήγησης..."
 	Start-Process "http://$domain"
+	Write-Host -ForegroundColor Green ' √'
 	
-	"`n`n`n`t Η διαδθκασία ολοκληρώθηκε!"
-	if ($debug) {pause}
+	"`n`n`n`t Η διαδικασία ολοκληρώθηκε!"
+	Write-Host -ForegroundColor Yellow "`n`t (Πατήστε οποιδήποτε πλήκτρο για να εξέλθετε)"
+	$NULL = [console]::ReadKey($true).Key
 	exit
 }
 
