9d1abb5d35c02f9f53077036ab40a196f3c472d4
CrazyRabbit <crazyrabbit0@outlook.com>
2023-11-23 23:48:10 +0200

    Create AutoLogin.ps1

---
+++ AutoLogin.ps1
@@ -0,0 +1,116 @@
+
+#-----------------------------------------------------------Administrator-----------------------------------------------------------#
+
+$debug = 0
+$has_admin_rights = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+if (-Not $has_admin_rights) {Start-Process 'powershell' '-NoProfile -ExecutionPolicy Bypass', $(if (Test-Path $MyInvocation.MyCommand.Definition -EA 0) {"-File `"$PSCommandPath`" $args"} else {$MyInvocation.MyCommand.Definition -replace '"', "'"}) -WorkingDirectory "$pwd" -Verb 'RunAs' -WindowStyle 'Normal'; if ($debug) {pause} exit}
+
+#-----------------------------------------------------------Variables-----------------------------------------------------------#
+
+$title	= 'AutoLogin Option'
+
+#-----------------------------------------------------------Main Code-----------------------------------------------------------#
+
+function main
+{
+	param ([String[]] $argz)
+	
+	show_title $title -set_title $TRUE
+	
+	$registry = @{
+		key		= 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device'
+		entry	= 'DevicePasswordLessBuildVersion'
+		value	= @{
+			enabled		= 0
+			disabled	= 2
+		}
+	}
+	
+	$opposite_state = @{
+		enabled		= 'disabled'
+		disabled	= 'enabled'
+	}
+	
+	$current_registry_value = (Get-ItemProperty -Path $registry.key).$($registry.entry)
+	$current_state = $registry.value.GetEnumerator().Where({$_.Value -eq $current_registry_value}).Name
+	
+	Write-Host -NoNewLine "`n`n`t $title is $current_state, do you wish to $($opposite_state.$current_state.TrimEnd('d')) it?  [y/n] "
+	do {
+		$user_choice = [Console]::ReadKey($TRUE).Key
+	} until (@('Y', 'N') -contains $user_choice)
+	""
+		
+
+	if ($user_choice -eq 'Y')
+	{
+		list_item ((Get-Culture).TextInfo.ToTitleCase($opposite_state.$current_state.TrimEnd('ed')) + "ing $title...")
+		Set-ItemProperty -Path $registry.key -Name $registry.entry -Value $registry.value.$($opposite_state.$current_state)
+		check_mark
+	
+		list_item 'Opening User Accounts...'
+		Start-Process 'netplwiz'
+		#Start-Process 'control' 'userpasswords2'	# Alternative
+		check_mark
+	}
+	else
+	{
+		list_item 'Operation Aborted...'
+		check_mark 'x' 'Red'
+	}
+	
+	finish
+}
+
+#-----------------------------------------------------------Functions-----------------------------------------------------------#
+
+function show_title
+{
+	param (
+        [Parameter(Mandatory)]
+        [string]$title,
+		
+		[bool]$set_title = $FALSE
+    )
+	
+	if ($set_title)
+	{
+		$Host.UI.RawUI.WindowTitle = $title
+	}
+	"`n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  $title  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
+function list_item
+{
+	param (
+        [Parameter(Mandatory)]
+        [string]$text,
+		
+        [string]$symbol = '-'
+    )
+	
+	Write-Host -NoNewLine -ForegroundColor Gray "`n`t $symbol $text "
+}
+
+function check_mark
+{
+	param (
+        [string]$symbol = 'v',
+		
+        [string]$color = 'Green'
+    )
+	
+	Write-Host -ForegroundColor $color $symbol
+}
+
+function finish
+{
+	""
+	show_title 'Process Finished'
+	Write-Host -ForegroundColor Yellow "`n`n`t`t`t    (Press any key to exit)"
+	$NULL = [Console]::ReadKey($TRUE)
+	exit
+}
+
+#-----------------------------------------------------------Run Main Code-----------------------------------------------------------#
+
+main $args
