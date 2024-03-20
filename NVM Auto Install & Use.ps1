e103450dfcd520d635403caf154e52e54e796c3e
crazyrabbit0 <crazyrabbit0@outlook.com>
2024-03-20 16:48:44 +0200

    Added Path update & package.json's Node.js version

--- NVM Auto Install & Use.ps1
+++ NVM Auto Install & Use.ps1
@@ -36,8 +36,9 @@ Function My-Cd {
         isCurrent   = $null
     }
     
-    $nvmrc = @{
-        path        = '.nvmrc'
+    $project = @{
+        package     = 'package.json'
+        nvmrc       = '.nvmrc'
         isFound     = $null
         version     = $null
         isInstalled = $null
@@ -76,29 +77,37 @@ Function My-Cd {
         $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
     }
     else {
-        # Get current NVM Version
+        # Get Current Node version
         if ($node.isPresent) {
             $node.current = (Get-Item $node.folder).target[0]
         }
     }
     
-    $nvmrc.isFound = Test-Path $nvmrc.path -PathType Leaf
-    if ($nvmrc.isFound) {
-        # If .nvmrc exists: Install/Use .nvmrc's Node.js version
-        $nvmrc.version      = Get-Content $nvmrc.path -First 1
-        $nvmrc.isInstalled  = ([array](Get-ChildItem $nvm.folder -Directory).Name -Match "^$($nvmrc.version)")[0]
-        if (-not $nvmrc.isInstalled) {
-            "Installing Node.js $($nvmrc.version)..."
-            Start-Process nvm "install $($nvmrc.version)" -WindowStyle Hidden -Wait
+    # If package.json exist: Set as Project's Node.js version
+    if (Test-Path $project.package -PathType Leaf) {
+        $packageNodeVersion = Select-String -Path $project.package -Pattern '"node":\s*"(.*?)[.x]*"'
+        $project.version = $(if ($packageNodeVersion) { 'v' + $packageNodeVersion.Matches.Groups[1].Value })
+    }
+    # If package.json isn't found & .nvmrc exist: Set as Project's Node.js version
+    if (!$project.version -and (Test-Path $project.nvmrc -PathType Leaf)) {
+        $project.version = Get-Content $project.nvmrc -First 1
+    }
+    
+    # If Project's Node.js version is set: Install/Use Project's Node.js version
+    if ($project.version) {
+        $project.isInstalled = ([array](Get-ChildItem $nvm.folder -Directory).Name -Match "^$($project.version)")[0]
+        if (-not $project.isInstalled) {
+            "Installing Node.js $($project.version)..."
+            Start-Process nvm "install $($project.version)" -WindowStyle Hidden -Wait
         }
-        $nvmrc.isCurrent = $(if ($node.isPresent) { $node.current.Contains($nvmrc.version) })
-        if (-not $nvmrc.isCurrent) {
-            "Using Node.js $($nvmrc.version)..."
-            Start-Process nvm "use $($nvmrc.version)" -Verb RunAs -WindowStyle Hidden -Wait
+        $project.isCurrent = $(if ($node.isPresent) { $node.current.Contains($project.version) })
+        if (-not $project.isCurrent) {
+            "Using Node.js $($project.version)..."
+            Start-Process nvm "use $($project.version)" -Verb RunAs -WindowStyle Hidden -Wait
         }
-     }
-     else {
-        # If .nvmrc doesn't exist: Install/Use Latest/Newest Node.js
+    }
+    # If Project's Node.js version isn't set: Install/Use Latest/Newest Node.js version
+    else {
         $node.newest = ([array](Get-ChildItem $nvm.folder -Directory).Name -Match '^v\d+')[-1]
         if (-not $node.newest) {
             'Downloading latest Node.js version...'
@@ -110,7 +119,11 @@ Function My-Cd {
             "Using Node.js $($node.newest)..."
             Start-Process nvm 'use newest' -Verb RunAs -WindowStyle Hidden -Wait
         }
-     }
+    }
+    
+    # Update Path Environment Veriable to match Current Node version
+    $node.current   = (Get-Item $node.folder).target[0]
+    $env:Path       = $env:Path.Replace($env:NVM_SYMLINK, $node.current)
 }
 
 # Override "cd" alias
