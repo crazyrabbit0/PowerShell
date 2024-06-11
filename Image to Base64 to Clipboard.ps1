8c7f613a0289e110ad9eadbae6b1ca3df2a3b143
crazyrabbit0 <crazyrabbit0@outlook.com>
2024-06-11 12:33:54 +0300

    Create Image to Base64 to Clipboard.ps1

---
+++ Image to Base64 to Clipboard.ps1
@@ -0,0 +1,37 @@
+
+############################## GLOBALS ##############################
+
+$global:debug	= 0
+$global:display = 'Normal'
+$global:title = 'Image to Base64 on Clipboard'
+$global:args = $args
+
+############################## MAIN CODE ##############################
+
+function main {
+	
+	if ($global:debug) { $global:args; '' }
+    
+    if ($global:args.Length -eq 0) {
+        $global:args += "$env:tmp\temp.png"
+        (Get-Clipboard -Format Image).Save($global:args[0])
+    }
+
+	Foreach ($arg in $global:args) {
+		if (Test-Path $arg -PathType Leaf) {
+			$base64 = [convert]::ToBase64String((get-content $arg -encoding byte))
+            $base64 | Set-Clipboard
+			if ($global:debug) {
+                $arg
+                $base64
+            }
+		}
+	}
+	
+	if ($global:debug) { ''; pause }
+	exit
+}
+
+############################## RUN MAIN CODE ##############################
+
+main
